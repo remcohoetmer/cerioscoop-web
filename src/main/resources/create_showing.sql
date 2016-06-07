26c3ed306010fc1a4884c14f1201434db90a3241
RonSanders <Ron.sanders@cerios.nl>
2016-06-07 17:20:58 +0200

    Refactored create_motionpicture.sql to create_showing.sql

--- src/main/resources/create_showing.sql
+++ src/main/resources/create_showing.sql
@@ -1,25 +1,25 @@
-DROP TABLE IF EXISTS `motionpicture`;
+DROP TABLE IF EXISTS `showing`;
 
-CREATE TABLE `motionpicture` (
-	`motionpicture_id` INT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `showing` (
+	`showing_id` INT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`film_id` INT(20) UNSIGNED NOT NULL,
 	`room_id` INT(20) UNSIGNED NOT NULL,
 	`premiere_date` DATE NOT NULL,
 	`premiere_time` TIME NOT NULL,
-	`last_motionpicture_date` DATE NOT NULL,
-	`last_motionpicture_time` TIME NOT NULL,
-	PRIMARY KEY (`motionpicture_id`)
+	`last_showing_date` DATE NOT NULL,
+	`last_showing_time` TIME NOT NULL,
+	PRIMARY KEY (`showing_id`)
 )
 
 COLLATE='utf8_bin'
 ENGINE=InnoDB;
 
-INSERT INTO `motionpicture`
-  (`film_id`, `room_id`, `premiere_date`, `premiere_time`, `last_motionpicture_date`, `last_motionpicture_time`)
+INSERT INTO `showing`
+  (`film_id`, `room_id`, `premiere_date`, `premiere_time`, `last_showing_date`, `last_showing_time`)
 VALUES
   ('1', '1', '2016-07-01', '21:00:00', '2016-07-09', '20:00:00'),
   ('2', '1', '2016-07-01', '21:00:00', '2016-07-09', '20:00:00'),
   ('2', '2', '2016-07-01', '21:00:00', '2016-07-09', '20:00:00'),
   ('3', '1', '2016-07-01', '21:00:00', '2016-07-09', '20:00:00');
   
-SELECT * FROM `motionpicture`;
+SELECT * FROM `showing`;
