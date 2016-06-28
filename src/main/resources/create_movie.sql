9f3cecea362ee73f41e1d445ce256b179470da81
RonSanders <Ron.sanders@cerios.nl>
2016-06-28 18:04:32 +0200

    Fixed movie domain by adding description, catergory and changed de name to title.

--- src/main/resources/create_movie.sql
+++ src/main/resources/create_movie.sql
@@ -1,10 +1,12 @@
 DROP TABLE IF EXISTS `movie`;
 CREATE TABLE `movie` (
 	`movie_id` INT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(255) NOT NULL COLLATE 'utf8_bin',
+	`category_id` INT(20) UNSIGNED NOT NULL,
+	`title` VARCHAR(255) NOT NULL COLLATE 'utf8_bin',
 	`minutes` INT(3) UNSIGNED NULL,
 	`movie_type` INT(1) UNSIGNED NULL,
 	`language` VARCHAR(50) NOT NULL COLLATE 'utf8_bin',
+	`description` VARCHAR(255) NOT NULL COLLATE 'utf8_bin',
 	PRIMARY KEY (`movie_id`)
 )
 
@@ -12,10 +14,10 @@ COLLATE='utf8_bin'
 ENGINE=InnoDB;
 
 INSERT INTO `movie`
-  (`name`, `minutes`, `movie_type`, `language`)
+  (`category_id`, `title`, `minutes`, `movie_type`, `language`, `description`)
 VALUES
-  ('The Legend of Tarzan (2016)', '90', '3', 'Nederlands'),
-  ('Tarzan the Ape Man (1932)', '90', '2', 'Engels'),
-  ('Tarzan (1999)', '120', '2', 'Engels');
+  ('1', 'The Legend of Tarzan (2016)', '90', '3', 'Dutch', 'A story about a boy that survived in the jungle.'),
+  ('1', 'Tarzan the Ape Man (1932)', '90', '2', 'English', 'A story about a boy that survived in the jungle.'),
+  ('3', 'Tarzan (1999)', '120', '2', 'English', 'A story about a boy that survived in the jungle.');
 
 SELECT * FROM `movie`;
