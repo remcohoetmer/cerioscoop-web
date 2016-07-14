db07bc3dc75ce02b71b2e631412505e2a2b93925
Cerios <mjgroothuis85@gmail.com>
2016-07-14 16:19:51 +0200

    5$ wager with Ron

--- src/test/java/nl/cerios/cerioscoop/service/AuthenticateUserTest.java
+++ src/test/java/nl/cerios/cerioscoop/service/AuthenticateUserTest.java
@@ -5,17 +5,19 @@ import org.junit.Test;
 
 import nl.cerios.cerioscoop.domain.Customer;
 import nl.cerios.cerioscoop.domain.Employee;
+import nl.cerios.cerioscoop.util.DateUtils;
 
 public class AuthenticateUserTest {
 
-	
+	final GeneralService generalService = new GeneralService();
+	final DateUtils dateUtils = new DateUtils();
 	
 	@Test
-	public void test() {
-		final GeneralService generalService = new GeneralService();
+	public void testAuthenticateUser() {
+		
 		Employee testUser = null;
 		Assert.assertEquals(false, generalService.authenticateUser(testUser));
-		Customer testCustomer = new Customer();
+		Customer testCustomer = new Customer(1, "Marcel", "Groothuis", "Manollo7G", "secret", "mjg@cerios.nl", dateUtils.getCurrentSqlDate(), dateUtils.getCurrentSqlTime());
 		Assert.assertEquals(true, generalService.authenticateUser(testCustomer));
 	}
 
