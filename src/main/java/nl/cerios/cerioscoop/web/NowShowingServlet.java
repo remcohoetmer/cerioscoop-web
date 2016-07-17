c1d5b151243b4ce36450d0a75b9f035da1d6781d
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-07-17 15:59:17 +0200

    Fixed NowShowing
    - corrected menubar URL
    - sort movies by date AND time
    - show page even if there is no 'firstShowing'

--- src/main/java/nl/cerios/cerioscoop/web/NowShowingServlet.java
+++ src/main/java/nl/cerios/cerioscoop/web/NowShowingServlet.java
@@ -1,21 +1,15 @@
 package nl.cerios.cerioscoop.web;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 import nl.cerios.cerioscoop.domain.Show;
 import nl.cerios.cerioscoop.service.GeneralService;
@@ -29,69 +23,57 @@ import nl.cerios.cerioscoop.util.DateUtils;
 @WebServlet("/NowShowingServlet")
 public class NowShowingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	@EJB //call DB
+
+	@EJB // call DB
 	private GeneralService generalService;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		final DateUtils dateUtils = new DateUtils();
+
 		final List<Show> shows = generalService.getShows();
 		final Show firstShowing = generalService.getFirstShowAfterCurrentDate();
-		Date showDate = null;
-		String showDateFromSqlDatabase = null;
-		 
-		if(firstShowing == null) {
-			return;	
-		}
-		 else{showDateFromSqlDatabase = dateUtils.sqlDatabaseFormat(firstShowing.getShowDate())+" "+dateUtils.timeFormat(firstShowing.getShowTime());
+
+		// Sort the shows by their showDate/showTime, oldest first.
+		shows.sort((itemL, itemR) -> {
+			int compare = itemL.getShowDate().compareTo(itemR.getShowDate());
+			if (compare == 0) {
+				compare = itemL.getShowTime().compareTo(itemR.getShowTime());
+			}
+			return compare;
+		});
+
+		if (firstShowing != null) {
+			// Second object in red box
+			final String firstUpcomingMovie;
 			try {
-				showDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(showDateFromSqlDatabase);
-			}catch (ParseException e) {
-			throw new ShowException("Something went wrong while parsing premiere datum.", e);
+				firstUpcomingMovie = generalService.getMovieByMovieId(firstShowing.getMovieId()).getTitle() + " on "
+						+ dateUtils.format2(firstShowing.getShowDate()) + " at "
+						+ dateUtils.timeFormat(firstShowing.getShowTime());
+			} catch (MovieNotFoundException e) {
+				throw new ServiceException("Something went terribly wrong while finding the movie.", e);
 			}
+
+			// Third object in red box
+			final Date showDateTime = DateUtils.toDateTime(firstShowing.getShowDate(), firstShowing.getShowTime());
+			final String countdown = dateUtils
+					.calculateTime(dateUtils.getSecondsBetween(showDateTime, dateUtils.getCurrentDate()));
+
+			// Objects to sent to the now-showing.jsp
+			request.setAttribute("first_upcoming_movie", firstUpcomingMovie);
+			request.setAttribute("countdown", countdown);
 		}
 
-		shows.sort(new Comparator<Show>() {
-
-			public int compare(final Show itemL, final Show itemR) {  //is itemL groter dan itemR? anders bovenaan
-				if (itemL.getShowDate().before(itemR.getShowDate())) {
-					return -1;
-				} else if (itemL.getShowDate().after(itemR.getShowDate())) {
-					return 1;
-				} else {
-					return 0;
-				}
-			}
-		});	
-		
-		// First object in red box
-		String todaysDate = dateUtils.getDate();
-		String firstUpcomingMovie = null;
-		
-		// Second object in red box
-		try {
-			firstUpcomingMovie = generalService.getMovieByMovieId(firstShowing.getMovieId()).getTitle() + " on " + dateUtils.format2(firstShowing.getShowDate())+" at "+ dateUtils.timeFormat(firstShowing.getShowTime());
-			System.out.println(firstUpcomingMovie);
-		} catch (MovieNotFoundException e) {
-			throw new ServiceException("Something went terribly wrong while finding the movie.", e);
-			
-		}
-		
-		// Third object in red box
-		String countdown = dateUtils.calculateTime(dateUtils.getSecondsBetween(showDate, dateUtils.getCurrentDate()));
-		
 		// Objects to sent to the now-showing.jsp
-		request.setAttribute("todays_date", todaysDate);
-		request.setAttribute("first_upcoming_movie", firstUpcomingMovie);
-		request.setAttribute("countdown", countdown);
+		request.setAttribute("todays_date", dateUtils.getDate());
 		request.setAttribute("shows", shows);
-	
+
 		// route to jsp
 		getServletContext().getRequestDispatcher("/jsp/now-showing.jsp").forward(request, response);
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
