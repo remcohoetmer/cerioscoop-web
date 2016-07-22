5b09ece052362a733b413fe54d8b0928e2c2dcc7
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-07-22 14:10:00 +0200

    Added default serialVersionUID's

--- src/main/java/nl/cerios/cerioscoop/service/MovieNotFoundException.java
+++ src/main/java/nl/cerios/cerioscoop/service/MovieNotFoundException.java
@@ -2,6 +2,8 @@ package nl.cerios.cerioscoop.service;
 
 public class MovieNotFoundException extends Exception {
 	
+	private static final long serialVersionUID = 1L;
+
 	public MovieNotFoundException(final String message) {
 		super(message);
 	}
