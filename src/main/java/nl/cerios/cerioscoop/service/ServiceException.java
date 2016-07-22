4802e00e73ad2795505a081722f5ecf1e40001cb
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-07-22 13:33:44 +0200

    Added default serialVersionUID

--- src/main/java/nl/cerios/cerioscoop/service/ServiceException.java
+++ src/main/java/nl/cerios/cerioscoop/service/ServiceException.java
@@ -1,6 +1,7 @@
 package nl.cerios.cerioscoop.service;
 
 public class ServiceException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 
 	public ServiceException(final String message) {
 		super(message);
--- src/main/java/nl/cerios/cerioscoop/web/ShowException.java
+++ src/main/java/nl/cerios/cerioscoop/web/ShowException.java
@@ -1,6 +1,8 @@
 package nl.cerios.cerioscoop.web;
 
 public class ShowException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
 
 	public ShowException(final String message) {
 	    super(message);
