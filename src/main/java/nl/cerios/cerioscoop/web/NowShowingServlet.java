f5287ca79e3f2bb69961325bccf8ce6bb295ddc5
RonSanders <Ron.sanders@cerios.nl>
2016-07-26 21:34:58 +0200

    Made the ShowingList actual with the NowShowingList

--- src/main/java/nl/cerios/cerioscoop/web/NowShowingServlet.java
+++ src/main/java/nl/cerios/cerioscoop/web/NowShowingServlet.java
@@ -1,6 +1,7 @@
 package nl.cerios.cerioscoop.web;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import nl.cerios.cerioscoop.domain.ShowingList;
+import nl.cerios.cerioscoop.domain.ShowingListBuilder;
 import nl.cerios.cerioscoop.service.GeneralService;
 import nl.cerios.cerioscoop.util.DateUtils;
 
@@ -29,6 +31,7 @@ public class NowShowingServlet extends HttpServlet {
 			throws ServletException, IOException {
 		final DateUtils dateUtils = new DateUtils();
 		final List<ShowingList> showingList = generalService.getShowingList();
+		final List<ShowingList> NowShowingList = new ArrayList<ShowingList>();
 		final ShowingList firstShowing = generalService.getFirstShowAfterCurrentDate(showingList);
 
 		// Sort the shows by their showDate/showTime, oldest first.
@@ -39,21 +42,32 @@ public class NowShowingServlet extends HttpServlet {
 			}
 			return compare;
 		});
-
+		
+		//Building the actualShowingList to show only the shows after the current date
+		for (ShowingList show : showingList){
+			if (show.getShowingListDate().after(dateUtils.getCurrentDate())){
+				show = new ShowingListBuilder()
+					.withShowingListId(show.getShowingListId())
+					.withMovieTitle(show.getMovieTitle())
+					.withRoomName(show.getRoomName())
+					.withShowingListDate(show.getShowingListDate())
+					.withShowingListTime(show.getShowingListTime())
+					.build();			
+				NowShowingList.add(show);
+			}
+		}
+		request.setAttribute("actualShows", NowShowingList);
+		
 		// Third object in red box
 		final Date showDateTime = DateUtils.toDateTime(firstShowing.getShowingListDate(),
 				firstShowing.getShowingListTime());
 		final String countdown = dateUtils
 				.calculateTime(dateUtils.getSecondsBetween(showDateTime, dateUtils.getCurrentDate()));
 
-		// // Objects to sent to the now-showing.jsp
+		// Objects to sent to the now-showing.jsp
 		request.setAttribute("first_upcoming_movie", firstShowing.getMovieTitle());
 		request.setAttribute("countdown", countdown);
-
-		// Objects to sent to the now-showing.jsp
 		request.setAttribute("todays_date", dateUtils.getDate());
-
-		request.setAttribute("shows", showingList);
 
 		// route to jsp
 		getServletContext().getRequestDispatcher("/jsp/now-showing.jsp").forward(request, response);
