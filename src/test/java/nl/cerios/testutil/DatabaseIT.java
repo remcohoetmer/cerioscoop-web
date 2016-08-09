bf1e2796572a53ace54364e234f40888dae04fef
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-08-09 23:36:34 +0200

    use Mockito to inject test beans

--- src/test/java/nl/cerios/testutil/DatabaseIT.java
+++ src/test/java/nl/cerios/testutil/DatabaseIT.java
@@ -2,10 +2,12 @@ package nl.cerios.testutil;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.SQLNonTransientConnectionException;
 import java.sql.Statement;
+import java.util.Properties;
 import java.util.Scanner;
 
 import javax.sql.DataSource;
@@ -13,57 +15,81 @@ import javax.sql.DataSource;
 import org.apache.derby.jdbc.EmbeddedDataSource40;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.mockito.Spy;
 
-public class DatabaseIT extends IntegrationTest {
+public class DatabaseIT {
+
+	private static final Properties PROPS;
+
+	static {
+		final URL propertyDataURL = DatabaseIT.class.getResource("/integration-test.properties");
+		try (final InputStream propertyDataStream = propertyDataURL.openStream()) {
+			PROPS = new Properties();
+			PROPS.load(propertyDataStream);
+		} catch (IOException e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+
+	@Spy
+	private DataSource dataSource = getDataSource();
+
+	private static DataSource getDataSource() {
+		final EmbeddedDataSource40 derbyDS = new EmbeddedDataSource40();
+		derbyDS.setDatabaseName("memory:" + PROPS.getProperty("db.name"));
+		derbyDS.setUser(PROPS.getProperty("db.user"));
+		derbyDS.setPassword(PROPS.getProperty("db.password"));
+		return derbyDS;
+	}
 
 	@BeforeClass
-	public static void startDatabase() throws IOException, SQLException {
+	public static void initDatabase() throws IOException, SQLException {
 		final DataSource ds = getDataSource();
+		// tell Derby to create the schema
 		((EmbeddedDataSource40) ds).setCreateDatabase("create");
-		try (final Connection conn = ds.getConnection();
-				final InputStream is1 = DatabaseIT.class.getResourceAsStream("/derby-schema.sql");
-				final InputStream is2 = DatabaseIT.class.getResourceAsStream("/derby-testdata.sql")) {
-			importSQL(conn, is1);
-			importSQL(conn, is2);
+
+		// run SQL scripts
+		try (final Connection connection = ds.getConnection();
+				final Statement sqlStatement = connection.createStatement()) {
+
+			final String[] scriptResourceNames = new String[]{
+				PROPS.getProperty("db.script.create"),
+				PROPS.getProperty("db.script.testData")
+			};
+			
+			for (String scriptResourceName : scriptResourceNames) {
+				try (InputStream scriptStream = DatabaseIT.class.getResourceAsStream(scriptResourceName)) {
+					runSqlScript(sqlStatement, scriptStream);
+				}
+			}
+		}
+	}
+
+	private static void runSqlScript(final Statement sqlStatement, final InputStream scriptInputStream)
+			throws SQLException {
+		try (final Scanner inputScanner = new Scanner(scriptInputStream)) {
+			inputScanner.useDelimiter(";");
+			while (inputScanner.hasNext()) {
+				final String line = inputScanner.next().trim();
+				if (!line.isEmpty()) {
+					sqlStatement.execute(line);
+				}
+			}
 		}
 	}
 
 	@AfterClass
-	public static void stopDatabase() throws SQLException {
+	public static void shutdownDatabase() throws SQLException {
 		try {
 			final DataSource ds = getDataSource();
 			((EmbeddedDataSource40) ds).setShutdownDatabase("shutdown");
 			ds.getConnection();
 		} catch (SQLNonTransientConnectionException e) {
+			// Wow, this is a dirty tricky piece of code! See
+			// http://stackoverflow.com/questions/2307788/how-to-shutdown-derby-in-memory-database-properly
 			final String dbState = e.getSQLState();
 			if (!dbState.equals("XJ015") && !dbState.equals("08006")) {
-				// See http://stackoverflow.com/questions/2307788/how-to-shutdown-derby-in-memory-database-properly
 				throw e;
-			}
-		}
-	}
-
-	public static DataSource getDataSource() {
-		final EmbeddedDataSource40 derbyDS = new EmbeddedDataSource40();
-		derbyDS.setDatabaseName("memory:" + getProperty("db.name"));
-		derbyDS.setUser(getProperty("db.user"));
-		derbyDS.setPassword(getProperty("db.password"));
-		return derbyDS;
-	}
-
-	protected static void importSQL(final Connection conn, final InputStream in) throws SQLException {
-		try (final Scanner s = new Scanner(in); final Statement st = conn.createStatement()) {
-			s.useDelimiter("(;(\r)?\n)|(--\n)");
-			while (s.hasNext()) {
-				String line = s.next();
-				if (line.startsWith("/*!") && line.endsWith("*/")) {
-					int i = line.indexOf(' ');
-					line = line.substring(i + 1, line.length() - " */".length());
-				}
-
-				if (line.trim().length() > 0) {
-					st.execute(line);
-				}
 			}
 		}
 	}
