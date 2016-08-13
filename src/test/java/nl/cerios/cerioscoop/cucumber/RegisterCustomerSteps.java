59841dbfb346a67f26bdad4923be2c08f0aada47
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-08-13 17:30:45 +0200

    quietly ignore Selenium tests
    MUCH better solution! All Selenium tests are now skipped (without
    failing) if the web server is not running or if no web driver is
    available.

--- src/test/java/nl/cerios/cerioscoop/cucumber/RegisterCustomerSteps.java
+++ src/test/java/nl/cerios/cerioscoop/cucumber/RegisterCustomerSteps.java
@@ -2,59 +2,56 @@ package nl.cerios.cerioscoop.cucumber;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import nl.cerios.cerioscoop.jsp.SeleniumTest;
+import nl.cerios.testutil.SeleniumTest;
 
 public class RegisterCustomerSteps extends SeleniumTest {
 
 	@Given("^I am on the cerioscoop site$")
 	public void navigateToCerioscoopSite() throws InterruptedException {
-		driver = new ChromeDriver();
-		driver.navigate().to(BASE_URL + "/index.jsp");
+		getWebDriver().navigate().to(BASE_URL + "/index.jsp");
 	}
 
 	@When("^I navigate to register$")
 	public void clickOnRegisterButton() throws InterruptedException {
-		driver.findElement(By.id("navbar-login")).click();
-		driver.findElement(By.id("register-button")).click();
+		getWebDriver().findElement(By.id("navbar-login")).click();
+		getWebDriver().findElement(By.id("register-button")).click();
 	}
 
 	@When("^I fill in the registerform with valid data and submit it$")
 	public void fillInRegisterFormAndSubmit() throws InterruptedException {
-		driver.findElement(By.id("firstname")).sendKeys("Selenium");
-		driver.findElement(By.id("lastname")).sendKeys("London");
-		driver.findElement(By.id("username")).sendKeys("Sel");
-		driver.findElement(By.id("password")).sendKeys("london");
-		driver.findElement(By.id("email")).sendKeys("selenium@london.com");
-		driver.findElement(By.id("submit")).click();
+		getWebDriver().findElement(By.id("firstname")).sendKeys("Selenium");
+		getWebDriver().findElement(By.id("lastname")).sendKeys("London");
+		getWebDriver().findElement(By.id("username")).sendKeys("Sel");
+		getWebDriver().findElement(By.id("password")).sendKeys("london");
+		getWebDriver().findElement(By.id("email")).sendKeys("selenium@london.com");
+		getWebDriver().findElement(By.id("submit")).click();
 	}
 
 	@When("^I navigate to the cerioscoop site$")
 	public void navigateToCerioscoopPage() throws InterruptedException {
-		driver.navigate().to(BASE_URL + "/index.jsp");
+		getWebDriver().navigate().to(BASE_URL + "/index.jsp");
 	}
 
 	@When("^I click on login$")
 	public void clickOnLoginButton() throws InterruptedException {
-		driver.findElement(By.id("navbar-login")).click();
+		getWebDriver().findElement(By.id("navbar-login")).click();
 	}
 
 	@When("^I fill in the username and password$")
 	public void loginWithUsernamePassword() throws InterruptedException {
-		driver.findElement(By.className("login-menu")).findElement(By.id("loginUsername")).sendKeys("Sel");
-		driver.findElement(By.className("login-menu")).findElement(By.id("loginPassword")).sendKeys("london");
-		driver.findElement(By.id("login-button")).click();
+		getWebDriver().findElement(By.className("login-menu")).findElement(By.id("loginUsername")).sendKeys("Sel");
+		getWebDriver().findElement(By.className("login-menu")).findElement(By.id("loginPassword")).sendKeys("london");
+		getWebDriver().findElement(By.id("login-button")).click();
 	}
 
 	@Then("^I check that the customer has been registered$")
 	public void checkIfCustomerIsRegistered() throws InterruptedException {
-		Assert.assertEquals("Hello customer!", driver.getTitle());
+		Assert.assertEquals("Hello customer!", getWebDriver().getTitle());
 		Assert.assertEquals("Login Successful!\nHello Sel!",
-				driver.findElement(By.className("login-message")).getText());
-		driver.quit();
+				getWebDriver().findElement(By.className("login-message")).getText());
 	}
 }
