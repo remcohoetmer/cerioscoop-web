cfd21e7610f7084fc0c827fabfe63a412260a98a
RonSanders <Ron.sanders@cerios.nl>
2016-08-17 12:02:21 +0200

    Implemented the PageObjectModel for selenium

---
+++ src/main/java/nl/cerios/cerioscoop/selenium/pages/CustomerPage.java
@@ -0,0 +1,21 @@
+package nl.cerios.cerioscoop.selenium.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CustomerPage {
+
+	@FindBy(className = "login-message")
+	private WebElement welcomeMessageParagraph;
+	
+	public CustomerPage(WebDriver driver) {
+	    // This call sets the WebElement fields.
+	    PageFactory.initElements(driver, this);
+	  }
+
+	  public String getWelcomeMessage() {
+	    return welcomeMessageParagraph.getText();
+	  }
+}
