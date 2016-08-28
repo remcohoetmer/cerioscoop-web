145379a371c91eafc274bbbfdef17e1c8752864f
Friso Schutte <friso.schutte@cerios.nl>
2016-08-28 17:17:57 +0200

    Compilatie fix in CustomerService

--- src/main/java/nl/cerios/cerioscoop/service/CustomerService.java
+++ src/main/java/nl/cerios/cerioscoop/service/CustomerService.java
@@ -33,21 +33,31 @@ public class CustomerService {
 			throw new ServiceException("Something went wrong while updating the chairamount.", e);
 		}
 	}
+	
+	
 	public int getChairsSoldAmount(){
 		
+		// TODO: wil je hier niet weten hoeveel stoelen per voorstelling ipv het totaal?
 		String SQL = "SELECT chairs_sold FROM show_table";
 		
-		try (final Connection connection = dataSource.getConnection()){
+		try (final Connection connection = dataSource.getConnection();
 			final Statement statement = connection.createStatement();
-			final ResultSet resultSet = statement.executeQuery(SQL); { 
-			while (resultSet.next()) {
-				final int chairsSold = resultSet.getInt("chairs_sold");
-				return chairsSold;
-					}
-			}
+			final ResultSet resultSet = statement.executeQuery(SQL)) {  
 			
-		}catch (final SQLException e) {
-	    	throw new ServiceException("Something went terribly wrong while retrieving the first date.", e);
+				// TODO: hoeveel rijen verwacht je eigenlijk dat hij teruggeeft?
+				// Als dit er altijd maar 1 kan zijn, dan heb je geen while loop nodig toch?
+				while (resultSet.next()) {
+					final int chairsSold = resultSet.getInt("chairs_sold");
+					
+					// TODO: altijd een beetje gek om een 'return' statement binnen een loop te hebben. Toch?
+					return chairsSold;
+				}
+			
+				// Hier komt hij dus als er geen 'next()' in de resultset is. Als er geen enkele record gevonden wordt dus.
+				throw new ServiceException("Niet mogelijk om aantal verkochte stoelen te bepalen, want record niet gevonden in de database");
+			
+		} catch (final SQLException e) {
+	    	throw new ServiceException("Something went terribly wrong while retrieving the amount of chairs sold.", e);
+		}
 	}
-}
 }
